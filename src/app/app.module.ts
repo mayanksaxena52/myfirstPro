e87dcf274171262a50aee65fa20e51c9d8ea8db1
mayanksaxena52 <mayank.saxena@infobeans.com>
2020-02-17 15:12:29 +0530

    4 files modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -6,10 +6,13 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login.component';
 import { RegisterComponent } from './register.component';
 import { AppRoutingModule } from './app.routing';
-import { AngularMaterialModule } from './angular-material.module';
+import { AngularMaterialModule } from './material.module';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,AppRoutingModule,ReactiveFormsModule,AngularMaterialModule ],
+  imports:      [ BrowserModule, FormsModule,AppRoutingModule,ReactiveFormsModule,AngularMaterialModule,BrowserAnimationsModule ],
   declarations: [ AppComponent ,LoginComponent,RegisterComponent],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
